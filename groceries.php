1b27355e996a4b09e7ad707e1c5bc3fc0ebda1e5
Ryan Rickgauer <rrickgauer1@gmail.com>
2019-12-07 20:44:47 -0600

    formatting

--- groceries.php
+++ groceries.php
@@ -11,41 +11,40 @@
 	<?php include('navbar.php'); ?>
 
 	<div class="container-fluid">
-		<h1>Test</h1>
+		<h1>Grocery Calculator</h1>
 
+		<div class="row">
 
-		<form class="form">
-			<input type="text" id="name" class="form-control" placeholder="Name"> <br>
-			<input type="number" id="price" class="form-control" placeholder="Price"> <br>
-			<input type="number" id="quantity" class="form-control" placeholder="Quantity"> <br>
-		</form>
+			<div id="add-item-section" class="col-sm-12 col-md-6">
+				<form class="form">
+					<input type="text" id="name" class="form-control item-input" placeholder="Name"> <br>
+					<input type="number" id="price" class="form-control item-input" placeholder="Price"> <br>
+					<input type="number" id="quantity" class="form-control item-input" placeholder="Quantity"> <br>
+				</form>
 
-		<br>
-
-		<button class="btn btn-primary" id="add-item-button">Add Item</button>
-
-
-		<button class="btn btn-success" type="button" data-toggle="collapse" data-target="#sum-section" aria-expanded="false" aria-controls="multiCollapseExample2">Show Sum</button>
-
-		<div id="sum-section" class="collapse col-sm-12 col-md-4 col-lg-3">
-
-			<div class="form-group">
-				<label for="tax"><b>Tax:</b></label>
-				<input type="number" class="form-control" id="tax" placeholder="Enter tax" value="0.1">
+				<button class="btn btn-primary" id="add-item-button">Add Item</button>
 			</div>
 
-			<div class="form-group">
-				<label for="additional-funds"><b>Additional Funds:</b></label>
-				<input type="text" class="form-control" id="additional-funds" placeholder="Enter additional funds" value="0">
+			<div id="sum-results" class="col-sm-12 col-md-6">
+				<button class="btn btn-success" type="button" data-toggle="collapse" data-target="#sum-section" aria-expanded="false" aria-controls="multiCollapseExample2" id="show-sum-button">Show Sum</button>
+				<div id="sum-section" class="collapse col-sm-12 col-md-4 col-lg-3">
+
+					<div class="form-group">
+						<label for="tax"><b>Tax:</b></label>
+						<input type="number" class="form-control" id="tax" placeholder="Enter tax" value="0.1">
+					</div>
+
+					<div class="form-group">
+						<label for="additional-funds"><b>Additional Funds:</b></label>
+						<input type="text" class="form-control" id="additional-funds" placeholder="Enter additional funds" value="0">
+					</div>
+
+					<div class="form-group">
+						<label for="sum"><b>Sum:</b></label>
+						<input type="text" class="form-control" id="sum" value="0" readonly>
+					</div>
+				</div>
 			</div>
-
-			<div class="form-group">
-				<label for="sum"><b>Sum:</b></label>
-				<input type="text" class="form-control" id="sum" value="0" readonly>
-			</div>
-
-
-
 
 		</div>
 
@@ -75,7 +74,6 @@
 </body>
 
 <script>
-
 	function addRow(name, price, quantity, totalCost) {
 		$('#table-body').append('<tr><td>' + name + '</td><td>' + price + '</td><td>' + quantity + '</td><td class="item-total-cost">' + totalCost + '</td></tr>');
 	}
@@ -90,11 +88,14 @@
 			var totalCost = price * quantity;
 
 			addRow(name, price, quantity, totalCost);
-			//$("input").val('');
+			$(".item-input").val('');
 
 			updateSum();
 
+		});
 
+		$("#show-sum-button").click(function() {
+			updateSum();
 		});
 
 	});
@@ -117,18 +118,6 @@
 
 		$("#sum").val('$' + newSum);
 	}
-
-
-
-
-
-
-
-
-
-
-
-
 </script>
 
 
