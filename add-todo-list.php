bac00635495e47b02d27eeb89c64a588d1f20cca
Ryan Rickgauer <rrickgauer1@gmail.com>
2019-12-18 16:12:15 -0600

    merge lists function

--- add-todo-list.php
+++ add-todo-list.php
@@ -19,28 +19,32 @@ if (isset($_POST['lists']) == false || count($_POST['lists']) == 0) {
 
 
 $pdo = dbConnect();
-
-$sql = "insert into ListItems (list_id, text, completed) SELECT 119, text, completed from ListItems where";
+$sql = "insert into ListItems (list_id, text, completed) SELECT $newID, text, completed from ListItems where";
 
 if (count($_POST['lists']) == 1) {
     $sql = $sql . ' list_id=' . $_POST['lists'][0];
 } else {
 
+    $sql = $sql . ' list_id=' . $_POST['lists'][0];
 
+    $count = 1;
+    while ($count < count($_POST['lists'])) {
 
+        $listID = $_POST['lists'][$count];
 
-
-
-
-
-
-
+        $sql = $sql . ' OR list_id=' . $listID;
+        $count++;
+    }
 }
 
+$results = $pdo->exec($sql);
 
+$pdo = null;
+$sql = null;
 
-
-
+$location = "Location: todo-lists.php?listID=$newID";
+header($location);
+exit;
 
 
 ?>
