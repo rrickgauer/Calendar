ebb1f3caecfe66ec660e4fb1033f510895acd9b2
Ryan Rickgauer <rrickgauer1@gmail.com>
2019-12-22 01:22:34 -0600

    tabs to spaces

--- todo-lists.php
+++ todo-lists.php
@@ -3,294 +3,294 @@
 <html lang="en" dir="ltr">
 
 <head>
-	<?php include('header.php'); ?>
-	<link rel="stylesheet" type="text/css" href="todo-style.css">
-	<link rel="stylesheet" type="text/css" href="style.css">
+  <?php include('header.php'); ?>
+  <link rel="stylesheet" type="text/css" href="todo-style.css">
+  <link rel="stylesheet" type="text/css" href="style.css">
 
-	<link href="https://cdn.jsdelivr.net/npm/select2@4.0.12/dist/css/select2.min.css" rel="stylesheet" />
-	<script src="https://cdn.jsdelivr.net/npm/select2@4.0.12/dist/js/select2.min.js"></script>
+  <link href="https://cdn.jsdelivr.net/npm/select2@4.0.12/dist/css/select2.min.css" rel="stylesheet" />
+  <script src="https://cdn.jsdelivr.net/npm/select2@4.0.12/dist/js/select2.min.js"></script>
 
-	<title>Todo's</title>
+  <title>Todo's</title>
 </head>
 
 <body>
-	<?php include('navbar.php'); ?>
-	<div class="wrapper">
+  <?php include('navbar.php'); ?>
+  <div class="wrapper">
 
-		<!-- sidebar -->
-		<nav id="sidebar">
-			<div class="sidebar-header">
-				<h5>Lists
-					<div class="float-right align-vertical">
-						<a href="#" data-toggle="modal" data-target="#new-todo-list-modal" id="new-list">
-							<ion-icon name="add" class="hover-blue" data-toggle="tooltip" data-placement="top" title="New list"></ion-icon>
-						</a>
-					</div>
-				</h5>
-			</div>
+    <!-- sidebar -->
+    <nav id="sidebar">
+      <div class="sidebar-header">
+        <h5>Lists
+          <div class="float-right align-vertical">
+            <a href="#" data-toggle="modal" data-target="#new-todo-list-modal" id="new-list">
+              <ion-icon name="add" class="hover-blue" data-toggle="tooltip" data-placement="top" title="New list"></ion-icon>
+            </a>
+          </div>
+        </h5>
+      </div>
 
-			<ul class="list-unstyled components">
-				<?php printTodoListGroup(); ?>
-			</ul>
+      <ul class="list-unstyled components">
+        <?php printTodoListGroup(); ?>
+      </ul>
 
-		</nav>
+    </nav>
 
-		<!-- list card  -->
-		<div id="content">
-			<div class="container-fluid">
+    <!-- list card  -->
+    <div id="content">
+      <div class="container-fluid">
 
-				<button type="button" id="sidebarCollapse" class="btn btn-primary">
-					Lists <ion-icon name="folder"></ion-icon>
-				</button>
+        <button type="button" id="sidebarCollapse" class="btn btn-primary">
+          Lists <ion-icon name="folder"></ion-icon>
+        </button>
 
-				<div id="todo-list-section">
-					<?php
+        <div id="todo-list-section">
+          <?php
                         if (isset($_GET['listID']))
                            printListItems($_GET['listID']);
                      ?>
-				</div>
-			</div>
-		</div>
+        </div>
+      </div>
+    </div>
 
 
-		<!-- new todo list modal -->
-		<div class="modal fade" id="new-todo-list-modal" tabindex="-1" role="dialog" aria-hidden="true">
-			<div class="modal-dialog modal-lg" role="document">
-				<div class="modal-content">
-					<div class="modal-header card-header">
-						<h5 class="modal-title" id="exampleModalLabel">New List</h5>
-						<button type="button" class="close custom-text-white" data-dismiss="modal">&times;</button>
-					</div>
+    <!-- new todo list modal -->
+    <div class="modal fade" id="new-todo-list-modal" tabindex="-1" role="dialog" aria-hidden="true">
+      <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+          <div class="modal-header card-header">
+            <h5 class="modal-title" id="exampleModalLabel">New List</h5>
+            <button type="button" class="close custom-text-white" data-dismiss="modal">&times;</button>
+          </div>
 
-					<!-- see add-todo-list.php -->
-					<div class="modal-body custom-bg-white">
-						<form class="form" method="post" id="new-todo-form" name="new-todo-list-form" action="add-todo-list.php">
-							<input type="text" name="list-name" id="new-todo-name-input" placeholder="List name" class="form-control" required autofocus><br>
-							<select id="list-dropdown" name="lists[]" multiple="multiple" style="width: 100%">
-								<?php printListDropdown(); ?>
-							</select>
-						</form>
-					</div>
+          <!-- see add-todo-list.php -->
+          <div class="modal-body custom-bg-white">
+            <form class="form" method="post" id="new-todo-form" name="new-todo-list-form" action="add-todo-list.php">
+              <input type="text" name="list-name" id="new-todo-name-input" placeholder="List name" class="form-control" required autofocus><br>
+              <select id="list-dropdown" name="lists[]" multiple="multiple" style="width: 100%">
+                <?php printListDropdown(); ?>
+              </select>
+            </form>
+          </div>
 
-					<div class="modal-footer custom-bg-white">
-						<input type="submit" value="Create" form="new-todo-list-form" onclick="addList()" class="btn btn-primary">
-					</div>
-				</div>
-			</div>
-		</div>
-
-
-		<!-- edit todo list name -->
-		<div class="modal fade" id="update-todo-list-name-modal">
-			<div class="modal-dialog">
-				<div class="modal-content">
-
-					<!-- Modal Header -->
-					<div class="modal-header card-header">
-						<h4 class="modal-title">Edit name</h4>
-						<button type="button" class="close custom-text-white" data-dismiss="modal">&times;</button>
-					</div>
-
-					<!-- Modal body -->
-					<div class="modal-body custom-bg-white">
-						<form class="form" action="update-todo-list-name.php?listID=<?php echo $_GET['listID']; ?>" method="post" id="update-todo-list-name-form">
-							<input type="text" name="update-todo-list-title" class="form-control">
-						</form>
-					</div>
-
-					<!-- Modal footer -->
-					<div class="modal-footer custom-bg-white">
-						<button type="button" class="btn btn-primary" onclick="saveTodoListName()">Save</button>
-					</div>
-
-				</div>
-			</div>
-		</div>
-
-	</div>
-
-	<script>
-		// executes addTodoItem when enter is pressed
-		$(document).on("keypress", "#add-item-text", function(e) {
-			if (e.keyCode == 13) {
-				e.preventDefault();
-				addTodoItem();
-			}
-		});
-
-		// set list dropdown to select2
-		$(document).ready(function() {
-			$('#list-dropdown').select2();
-		});
-
-		function addTodoItem() {
-			// update list card
-			var xhttp = new XMLHttpRequest();
-			xhttp.onreadystatechange = function() {
-				if (this.readyState == 4 && this.status == 200) {
-					var e = this.responseText;
-					$("#todo-list-section").html(e);
-				}
-			};
+          <div class="modal-footer custom-bg-white">
+            <input type="submit" value="Create" form="new-todo-list-form" onclick="addList()" class="btn btn-primary">
+          </div>
+        </div>
+      </div>
+    </div>
 
 
-			var listID = $("#todo-list-card").attr("data-listid");
-			var text = $("#add-item-text").val();
+    <!-- edit todo list name -->
+    <div class="modal fade" id="update-todo-list-name-modal">
+      <div class="modal-dialog">
+        <div class="modal-content">
 
-			xhttp.open("GET", "add-todo-item.php?listID=" + listID + "&text=" + text, true);
-			xhttp.send();
+          <!-- Modal Header -->
+          <div class="modal-header card-header">
+            <h4 class="modal-title">Edit name</h4>
+            <button type="button" class="close custom-text-white" data-dismiss="modal">&times;</button>
+          </div>
 
-			$("#add-item-text").focus();
-		}
+          <!-- Modal body -->
+          <div class="modal-body custom-bg-white">
+            <form class="form" action="update-todo-list-name.php?listID=<?php echo $_GET['listID']; ?>" method="post" id="update-todo-list-name-form">
+              <input type="text" name="update-todo-list-title" class="form-control">
+            </form>
+          </div>
 
-		function updateComplete(checkbox) {
-			// update list card
-			var xhttp = new XMLHttpRequest();
-			xhttp.onreadystatechange = function() {
-				if (this.readyState == 4 && this.status == 200) {
-					var e = this.responseText;
-					$("#todo-list-section").html(e);
-				}
-			};
+          <!-- Modal footer -->
+          <div class="modal-footer custom-bg-white">
+            <button type="button" class="btn btn-primary" onclick="saveTodoListName()">Save</button>
+          </div>
 
-			var listID = $("#todo-list-card").attr("data-listid");
-			var itemID = $(checkbox).closest("tr").attr("data-itemID");
+        </div>
+      </div>
+    </div>
 
-			if (checkbox.checked == true) {
-				var completed = 'y';
-			} else {
-				var completed = 'n';
-			}
+  </div>
 
+  <script>
+    // executes addTodoItem when enter is pressed
+    $(document).on("keypress", "#add-item-text", function(e) {
+      if (e.keyCode == 13) {
+        e.preventDefault();
+        addTodoItem();
+      }
+    });
 
-			var link = 'update-todo-item-completed.php?listID=' + listID + '&itemID=' + itemID + '&completed=' + completed;
-			xhttp.open("GET", link, true);
-			xhttp.send();
-		}
+    // set list dropdown to select2
+    $(document).ready(function() {
+      $('#list-dropdown').select2();
+    });
 
-
-
-		function deleteTodoListItem(item) {
-
-			if (confirm("Are you sure you want to delete?")) {
-				// update list card
-				var xhttp = new XMLHttpRequest();
-				xhttp.onreadystatechange = function() {
-					if (this.readyState == 4 && this.status == 200) {
-						var e = this.responseText;
-						$("#todo-list-section").html(e);
-					}
-				};
-
-				var listID = $("#todo-list-card").attr("data-listid");
-				var itemID = $(item).closest("tr").attr("data-itemID");
-
-				var link = 'delete-todo-item.php?listID=' + listID + '&itemID=' + itemID;
-
-				xhttp.open("GET", link, true);
-				xhttp.send();
-			}
-		}
-
-		function completeAllListItems() {
-
-			var xhttp = new XMLHttpRequest();
-			xhttp.onreadystatechange = function() {
-				if (this.readyState == 4 && this.status == 200) {
-					var e = this.responseText;
-					$("#todo-list-section").html(e);
-				}
-			};
-
-			var listID = $("#todo-list-card").attr("data-listid");
-
-			var link = 'complete-all-list-items.php?listID=' + listID;
-
-			xhttp.open("GET", link, true);
-			xhttp.send();
-		}
-
-		function incompleteAllListItems() {
-
-			var xhttp = new XMLHttpRequest();
-			xhttp.onreadystatechange = function() {
-				if (this.readyState == 4 && this.status == 200) {
-					var e = this.responseText;
-					$("#todo-list-section").html(e);
-				}
-			};
-
-			var listID = $("#todo-list-card").attr("data-listid");
-
-			var link = 'incomplete-all-list-items.php?listID=' + listID;
-
-			xhttp.open("GET", link, true);
-			xhttp.send();
-		}
-
-		function addList() {
-			$("#new-todo-form").submit();
-		}
-
-		function updateListName() {
-			var listName = $("#list-title").text();
-			$("input[name=update-todo-list-title]").val(listName);
-		}
-
-		function saveTodoListName() {
-			$("#update-todo-list-name-form").submit();
-		}
-
-		function deleteTodoList(listID) {
-			if (confirm("Are you sure you want to delete this list?")) {
-				var link = 'delete-todo-list.php?listID=' + listID;
-				window.location.href = link;
-			}
-		}
-
-		function deleteCompletedListItems() {
+    function addTodoItem() {
+      // update list card
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          var e = this.responseText;
+          $("#todo-list-section").html(e);
+        }
+      };
 
 
-			// update list card
-			var xhttp = new XMLHttpRequest();
-			xhttp.onreadystatechange = function() {
-				if (this.readyState == 4 && this.status == 200) {
-					var e = this.responseText;
-					$("#todo-list-section").html(e);
-				}
-			};
+      var listID = $("#todo-list-card").attr("data-listid");
+      var text = $("#add-item-text").val();
+
+      xhttp.open("GET", "add-todo-item.php?listID=" + listID + "&text=" + text, true);
+      xhttp.send();
+
+      $("#add-item-text").focus();
+    }
+
+    function updateComplete(checkbox) {
+      // update list card
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          var e = this.responseText;
+          $("#todo-list-section").html(e);
+        }
+      };
+
+      var listID = $("#todo-list-card").attr("data-listid");
+      var itemID = $(checkbox).closest("tr").attr("data-itemID");
+
+      if (checkbox.checked == true) {
+        var completed = 'y';
+      } else {
+        var completed = 'n';
+      }
 
 
-			var listID = $("#todo-list-card").attr("data-listid");
-
-			xhttp.open("GET", "delete-completed-todo-list-item.php?listID=" + listID, true);
-			xhttp.send();
-
-		}
+      var link = 'update-todo-item-completed.php?listID=' + listID + '&itemID=' + itemID + '&completed=' + completed;
+      xhttp.open("GET", link, true);
+      xhttp.send();
+    }
 
 
 
-		// collapses side nav
-		$(document).ready(function() {
-			$('#sidebarCollapse').on('click', function() {
-				$('#sidebar').toggleClass('active');
-			});
-		});
+    function deleteTodoListItem(item) {
+
+      if (confirm("Are you sure you want to delete?")) {
+        // update list card
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+            var e = this.responseText;
+            $("#todo-list-section").html(e);
+          }
+        };
+
+        var listID = $("#todo-list-card").attr("data-listid");
+        var itemID = $(item).closest("tr").attr("data-itemID");
+
+        var link = 'delete-todo-item.php?listID=' + listID + '&itemID=' + itemID;
+
+        xhttp.open("GET", link, true);
+        xhttp.send();
+      }
+    }
+
+    function completeAllListItems() {
+
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          var e = this.responseText;
+          $("#todo-list-section").html(e);
+        }
+      };
+
+      var listID = $("#todo-list-card").attr("data-listid");
+
+      var link = 'complete-all-list-items.php?listID=' + listID;
+
+      xhttp.open("GET", link, true);
+      xhttp.send();
+    }
+
+    function incompleteAllListItems() {
+
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          var e = this.responseText;
+          $("#todo-list-section").html(e);
+        }
+      };
+
+      var listID = $("#todo-list-card").attr("data-listid");
+
+      var link = 'incomplete-all-list-items.php?listID=' + listID;
+
+      xhttp.open("GET", link, true);
+      xhttp.send();
+    }
+
+    function addList() {
+      $("#new-todo-form").submit();
+    }
+
+    function updateListName() {
+      var listName = $("#list-title").text();
+      $("input[name=update-todo-list-title]").val(listName);
+    }
+
+    function saveTodoListName() {
+      $("#update-todo-list-name-form").submit();
+    }
+
+    function deleteTodoList(listID) {
+      if (confirm("Are you sure you want to delete this list?")) {
+        var link = 'delete-todo-list.php?listID=' + listID;
+        window.location.href = link;
+      }
+    }
+
+    function deleteCompletedListItems() {
+
+
+      // update list card
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          var e = this.responseText;
+          $("#todo-list-section").html(e);
+        }
+      };
+
+
+      var listID = $("#todo-list-card").attr("data-listid");
+
+      xhttp.open("GET", "delete-completed-todo-list-item.php?listID=" + listID, true);
+      xhttp.send();
+
+    }
+
+
+
+    // collapses side nav
+    $(document).ready(function() {
+      $('#sidebarCollapse').on('click', function() {
+        $('#sidebar').toggleClass('active');
+      });
+    });
 
 
 
 
-		// sets the current side nav item to active
-		$(document).ready(function() {
-			var listID = $("#todo-list-card").data("listid");
-			$("a[data-listid=" + listID + "]").closest("li").addClass("active");
-		});
+    // sets the current side nav item to active
+    $(document).ready(function() {
+      var listID = $("#todo-list-card").data("listid");
+      $("a[data-listid=" + listID + "]").closest("li").addClass("active");
+    });
 
-		// set the background on the navbar to selected
-		$(document).ready(function() {
-			$("#todo-navbar-link").addClass("custom-bg-grey");
-		});
-	</script>
+    // set the background on the navbar to selected
+    $(document).ready(function() {
+      $("#todo-navbar-link").addClass("custom-bg-grey");
+    });
+  </script>
 
 </body>
 
@@ -300,16 +300,16 @@
 
 // prints the list dropdown menu in the new list modal
 function printListDropdown() {
-	$pdo = dbConnect();
+  $pdo = dbConnect();
 
-	$sql = "SELECT Lists.id, Lists.title FROM Lists ORDER BY Lists.title";
-	$results = $pdo->query($sql);
+  $sql = "SELECT Lists.id, Lists.title FROM Lists ORDER BY Lists.title";
+  $results = $pdo->query($sql);
 
-	while ($row = $results->fetch(PDO::FETCH_ASSOC)) {
-		$id = $row['id'];
-		$title = $row['title'];
-		echo "<option value=\"$id\">$title</option>";
-	}
+  while ($row = $results->fetch(PDO::FETCH_ASSOC)) {
+    $id = $row['id'];
+    $title = $row['title'];
+    echo "<option value=\"$id\">$title</option>";
+  }
 }
 
 ?>
