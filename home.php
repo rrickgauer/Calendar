0a8c94e5dd80a67f6fd04248d129e76bdb3e9d18
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-01-04 15:47:34 -0600

    add home.php

---
+++ home.php
@@ -0,0 +1,43 @@
+<?php include('functions.php'); ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <?php include('header.php'); ?>
+    <title>Home</title>
+  </head>
+  <body>
+    <?php include('navbar.php'); ?>
+    <div class="container-fluid">
+
+      <h1>Home</h1>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+
+    <script>
+    // set the background on the navbar to selected
+    $(document).ready(function() {
+      $("#home-navbar-link").addClass("custom-bg-grey");
+    });
+
+    </script>
+
+  </body>
+</html>
--- navbar.php
+++ navbar.php
@@ -11,6 +11,8 @@
       <!-- Links -->
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="nav navbar-nav">
+          <li class="nav-item nav-link-hover" id="home-navbar-link"><a class="nav-link " href="home.php">Home</a></li>
+
           <li class="nav-item dropdown" id="class-navbar-link">
             <a class="nav-link dropdown-toggle nav-link-hover" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Classes</a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -29,7 +31,7 @@
           <li class="nav-item nav-link-hover" id="add-class-navbar-link"><a class="nav-link nav-link-hover" href="add-class.php">Add Class</a></li>
           <li class="nav-item nav-link-hover dropdown" id="todo-navbar-link"><a href="todo-lists.php" class="nav-link nav-link-hover">Todo's</a></li>
           <li class="nav-item nav-link-hover" id="calendar-navbar-link"><a class="nav-link nav-link-hover" href="calendar-list.php">Calendar</a></li>
-          <li class="nav-item nav-link-hover" id="sets-navbar-link"><a class="nav-link nav-link-hover" href="sets.php">Study Sets</a></li>
+          <!-- <li class="nav-item nav-link-hover" id="sets-navbar-link"><a class="nav-link nav-link-hover" href="sets.php">Study Sets</a></li> -->
           <li class="nav-item nav-link-hover" id="groceries-navbar-link"><a class="nav-link nav-link-hover" href="groceries.php">Groceries</a></li>
           <!-- <li class="nav-item nav-link-hover" id="test-navbar-link"><a class="nav-link nav-link-hover" href="test.php">Test Page</a></li> -->
 
