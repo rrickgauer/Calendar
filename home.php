f464978096e8a12d7aa29a57b36cbdae07fd8021
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-01-05 22:18:21 -0600

    css

--- home.php
+++ home.php
@@ -24,6 +24,7 @@ $classes = getAllClassData();
     <div class="row">
 
       <div class="col-sm-12 col-md-6">
+        <!-- class cards -->
         <div class="card">
           <div class="card-header">
             <h2>Classes</h2>
@@ -33,7 +34,6 @@ $classes = getAllClassData();
             <?php
 
             $lineCount = 0;
-
             echo '<div class="row">';
 
             while ($class = $classes->fetch(PDO::FETCH_ASSOC)) {
@@ -58,33 +58,24 @@ $classes = getAllClassData();
                 $termDisplay = 'Summer 2019';
               }
 
-              echo "<div class=\"card class-card-home col-md-12 col-lg-4\" data-id=\"$id\">";
+              echo '<div class="col-md-12 col-lg-12 col-xl-4">';
+              echo "<div class=\"card class-card-home\" data-id=\"$id\">";
               echo '<div class="card-body">';
               echo "<h5>$dept-$number: $title </h5>";
               echo '<p>';
               echo "<span class=\"badge badge-secondary\">$termDisplay</span> ";
               echo "<span class=\"badge badge-info\">$count items</span>";
               echo '</p>';
-              echo '</div></div>';
+              echo '</div></div></div>';
 
               $lineCount++;
             }
 
             echo '</div>'
-
-
-
-
             ?>
-
-
-
           </div>
-
         </div>
-
       </div>
-
 
       <div class="col-sm-12 col-md-6">
         <!-- data summary -->
@@ -141,9 +132,6 @@ $classes = getAllClassData();
         </div>
 
       </div>
-
-
-
 
     </div>
   </div>
